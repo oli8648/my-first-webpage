fe5c43282308d1fec57e02ae061f03012a1df06e
Oliver <oliverquarmby@icloud.com>
2025-03-15 10:50:56 +1100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script>
+  import Block from './components/Block.vue'
+  import Results from './components/Results.vue'
+  export default {
+    name: "App",
+    components: { Block, Results },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false
+      }
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        this.showResults = false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+  #app {
+    text-align: center;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+</style>
